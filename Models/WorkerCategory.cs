a419a035177ea65ba1d8a8841aae21040dccd83e
Unity-2D-Cube <krks898ivanovic@gmail.com>
2022-11-27 18:19:23 +0100

    Add project files.

---
+++ Models/WorkerCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test_Projekat_Web.Models
+{
+    public class WorkerCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Ime { get; set; }
+        [Required]
+        public string Prezime { get; set; }
+        [Required]
+        public string RadnaPozicija { get; set; }
+        public int NetoPlata { get; set; }
+        [Required]
+        public string BrutoPlata { get; set; }
+        public string SiteVersion { get; set; } = "0.1(TechDemo)";
+        public DateTime DatumKreiranja { get; set; } = DateTime.Now;
+    }
+}
