66a67bd3732de5c937b56987c37281c99033262f
Unity-2D-Cube <krks898ivanovic@gmail.com>
2022-12-08 12:53:58 +0100

    error message form implementation

--- Controllers/EmployeeCategoryController.cs
+++ Controllers/EmployeeCategoryController.cs
@@ -8,7 +8,7 @@ namespace Test_Projekat_Web.Controllers
     {
         private readonly ApplicationDbContext _db;
 
-
+        
         public EmployeeCategoryController(ApplicationDbContext db)
         {
             _db = db;
@@ -32,9 +32,16 @@ namespace Test_Projekat_Web.Controllers
         [AutoValidateAntiforgeryToken]
         public IActionResult Create(EmployeeCategory obj)
         {
-            obj.NetoPlata_RSD = obj.BrutoPlata_RSD;
+            int i = 0123456789;
+            
+            if (obj.Ime == i.ToString("0123456789"))
+            {
+                ModelState.AddModelError("CustomError", "PAŽNJA! Ovo je nevažeći unos! Pokušajte ponovo bez unosa brojeva!");
+            }
+
             if (ModelState.IsValid)
-            {            
+            {
+            obj.NetoPlata_RSD = obj.BrutoPlata_RSD;    
             _db.EmployeeCategories.Add(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
